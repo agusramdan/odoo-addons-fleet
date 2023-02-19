7189b300c8c9ec25b602ae8a152e83a50f77d214
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2023-02-20 04:55:48 +0700

    Commit Fleet Vehicle Reserve

--- fleet_vehicle_reserve/__manifest__.py
+++ fleet_vehicle_reserve/__manifest__.py
@@ -1,33 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Fleet Vehicle Reserve",
-
     'summary': """
-        Reserve Vehicle by Time """,
-
-    'description': """
-        Reserve Vehicle by Time
-    """,
-
+        This module extends the Fleet module allowing the registration
+        of Reservation Vehicle by Time """,
     'author': "Agus Muhammad Ramdan",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Service',
+    'website': "https://github.com/agusramdan/odoo-addons-fleet",
+    "category": "Human Resources",
     'version': '14.0.1.0',
-
-    # any module necessary for this one to work correctly
+    "license": "AGPL-3",
     'depends': ['base', 'fleet'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'security/fleet_vehicle_reserve_rule.xml',
         'views/fleet_vehicle_reserved_views.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
